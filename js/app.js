ce61dc6f7a77a137b38553d8442a6124f6270a63
Geronimo Algieri <algierigeronimo1@gmail.com>
2023-01-14 15:34:49 -0300

    agregando sjson

--- js/app.js
+++ js/app.js
@@ -1,39 +1,23 @@
-// Login y json
-
-// let user = document.querySelector("#username")
-// let pass = document.querySelector("#pass")
-// let boton = document.querySelector("#button")
-
-// const logear = button.addEventListener("click", function(e){
-//     e.preventDefault()
-//     window.location = "productos.html"
-//     const data = {
-//         user: username.value,
-//         pass: pass.value
-//     }
-//     console.log(data)
-// })
-// Array de productos para guardar al localstorage
-const modelos = ["Sudadera con Capucha, Scuderia Mercedes AMG","Remera, Scuderia Mercedes AMG","Gorra de béisbol, Scuderia Mercedes AMG","Chaqueta acolchada ligera, Scuderia Mercedes AMG","Remera Polo, equipo Red Bull Racing","Gorra de béisbol, Scuderia Red Bull Racing","Sudadera con capucha, Scuderia Red Bull Racing","Softshell, Scuderia Red Bull Racing","Remera, Scuderia Ferrari Team Carlos Sainz","Sudadera con capucha negra, Scuderia Ferrari","Remera, Scuderia Ferrari Team Charles Leclerc","Sudadera con capucha, Scuderia Ferrari"]
-// // Guardar en LocalStorage
-
-const aJson = JSON.stringify(modelos)
-localStorage.setItem("Modelos de ropa", aJson)
-
-const contenedorProductos = document.getElementById(`contenedor-productos`)
+const productos = document.getElementById(`carrito-contenedor`)
 
 // tabla de productos
+fetch("/data.json")
+.then((resp)=>resp.json())
+.then((data)=>{
 
-const armarTabla = (prod) =>{
-    return `<div class="card" style="width: 18rem;">
-                <img src="${prod.img}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${prod.nombre}</h5>
-                    <p class="card-text">Precio:$${prod.precio}</p>
-                    <button type="button" class="btn btn-primary">Agregar</button>
-                </div>
-            </div>`
-}
+    data.forEach((prod)=>{
+    const div = document.createElement("div")
+    div.innerHTML=`<div class="card" style="width: 18rem;">
+                    <img src="${prod.img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${prod.nombre}</h5>
+                        <p class="card-text">Precio:$${prod.precio}</p>
+                        <button type="button" class="btn btn-primary">Agregar</button>
+                    </div>
+                </div>`
+    productos.appendChild(div)
+    })
+})
 
 // Plasmo los productos en la pagina
 
@@ -43,7 +27,7 @@ const cargarProductos = (array)=> {
         array.forEach(prod => {
             tabla += armarTabla(prod)
         });
-        contenedorProductos.innerHTML = tabla
+        productos.innerHTML = tabla
     }
 }
 cargarProductos(productos)
@@ -109,35 +93,13 @@ function limpiarHTML() {
 
 // Eliminar producto del carrito
 
-const vaciarCarrito = (prodId) => {
-    const item = articulosCarrito.find((prod) => prod.titulo === prodId)
-    const indice = articulosCarrito.indexOf(item)
-    articulosCarrito.splice(indice, 1)
-    
-    limpiarHTML()
-}
-
-
-// let nombre = document.querySelector("#nombre")
-// let email = document.querySelector("#email")
-
-// nombre.addEventListener("input", function () {
-//     if (nombre.value === "") {
-//         e.preventDefault
-//         info.innerHTML = `
-//         <div class="alert alert-danger" role="alert">
-//         <h5>Ingrese un nombre valido</h5>
-//         `
+// function eliminarProducto(e) {
+//     if (e.target.classList.contains("btn-danger")) {
+//       let productoID = e.target.getAttribute("id");
+//       articulosCarrito = articulosCarrito.filter(
+//         (prod) => prod.id !== prodID
+//       );
+//       carritoHTML();
 //     }
-// });
-
-// email.addEventListener("input", function () {
-//     if (email.value === "") {
-//         e.preventDefault
-//         info.innerHTML = `
-//         <div class="alert alert-danger" role="alert">
-//         <h5>Ingrese un nombre valido</h5>
-//         `
-//     }
-// });
+//   }
 
